670e1e4684a938a9b063da63796f96393b15ded0
Aslanch1k <zxerfgty@gmail.com>
2022-10-24 00:29:01 +0300

    Logic of app are ready

--- backendproj/views.py
+++ backendproj/views.py
@@ -1,88 +1,38 @@
 from flask import jsonify, request
-from uuid import uuid1
 from backendproj import app
 from datetime import datetime
-
-user_id_counter = 0
-def user_id_counter_plus_one():
-    global user_id_counter
-    user_id_counter += 1
-    return user_id_counter
-
-USERS = [
-    {
-        "User id": user_id_counter_plus_one(),
-        "User name": "Oleg",
-    },
-    {
-        "User id": user_id_counter_plus_one(),
-        "User name": "Masha",
-    },
-    {
-        "User id": user_id_counter_plus_one(),
-        "User name": "Artem",
-    },
-]
-
-categories_id_counter = 0
-def categories_id_counter_plus_one():
-    global categories_id_counter
-    categories_id_counter += 1
-    return categories_id_counter
-
-CATEGORIES = [
-    {
-        "Category id": categories_id_counter_plus_one(),
-        "Category name": "Shoes",
-    },
-    {
-        "Category id": categories_id_counter_plus_one(),
-        "Category name": "Shirts",
-    },
-    {
-        "Category id": categories_id_counter_plus_one(),
-        "Category name": "T-shirts",
-    }
-]
-
-now = datetime.now()
-records_id_counter = 0
-def records_id_counter_plus_one():
-    global records_id_counter
-    records_id_counter += 1
-    return records_id_counter
-
-RECORDS = [
-    {"Record id": records_id_counter_plus_one(),
-     "User id": USERS[0]["User id"],
-     "Category id": CATEGORIES[0]["Category id"],
-     "Date and time of record": now.strftime("%d/%m/%Y %H:%M:%S"),
-     "Amount of expenditure in USD": 500
-     },
-    {"Record id": records_id_counter_plus_one(),
-     "User id": USERS[1]["User id"],
-     "Category id": CATEGORIES[1]["Category id"],
-     "Date and time of record": now.strftime("%d/%m/%Y %H:%M:%S"),
-     "Amount of expenditure in USD": 320
-     },
-    {"Record id": records_id_counter_plus_one(),
-     "User id": USERS[0]["User id"],
-     "Category id": CATEGORIES[1]["Category id"],
-     "Date and time of record": now.strftime("%d/%m/%Y %H:%M:%S"),
-     "Amount of expenditure in USD": 250
-     },
-    {"Record id": records_id_counter_plus_one(),
-     "User id": USERS[1]["User id"],
-     "Category id": CATEGORIES[0]["Category id"],
-     "Date and time of record": now.strftime("%d/%m/%Y %H:%M:%S"),
-     "Amount of expenditure in USD": 730
-     }
-]
+from sort_defs import sort_for_category, sort_user_records
+from data_file import USERS,CATEGORIES,RECORDS
+from counters import user_id_counter_plus_one, categories_id_counter_plus_one, records_id_counter_plus_one
 
 
 @app.route("/categories")
 def get_categories():
     return jsonify({"Categories": CATEGORIES})
+
+
+user1 = USERS[0]
+
+
+@app.route(f"/user{user1['User id']}records")
+def get_user_records():
+    global user1
+    username = user1["User name"]
+    return jsonify({f"User {username} records": sort_user_records(RECORDS, user1)})
+
+
+user2 = USERS[1]
+category = CATEGORIES[0]
+
+
+@app.route(f"/user{user2['User id']}recordsincategory{category['Category id']}")
+def get_user_records_in_category():
+    global user2, category, RECORDS
+    username = user2["User name"]
+    categoryname = category["Category name"]
+    return jsonify({f"User {username} records in category {categoryname}":
+                        sort_for_category(category, sort_user_records(RECORDS, user2))})
+
 
 @app.route("/category", methods=['POST'])
 def create_category():
@@ -92,6 +42,7 @@ def create_category():
     print(CATEGORIES)
     return jsonify({"Status": "ok"})
 
+
 @app.route("/user", methods=['POST'])
 def create_user():
     request_data = request.get_json()
@@ -100,14 +51,18 @@ def create_user():
     print(USERS)
     return jsonify({"Status": "ok"})
 
-@app.route("/record", methods = ['POST'])
+
+@app.route("/record", methods=['POST'])
 def create_record():
     request_data = request.get_json()
-    request_data["Record id"] = records_id_counter_plus_one()
-    now = datetime.now()
-    request_data["Date and time of record"] = now.strftime("%d/%m/%Y %H:%M:%S")
-    if (request_data["User id"] > user_id_counter) or (request_data["Category id"] > categories_id_counter):
+    if (request_data["User id"] > USERS[-1]["User id"]) or \
+            (request_data["Category id"] > CATEGORIES[-1]["Category id"]):
+
         return IndexError
-    RECORDS.append(request_data)
-    print(RECORDS)
-    return jsonify({"Status": "ok"})
+    else:
+        request_data["Record id"] = records_id_counter_plus_one()
+        now = datetime.now()
+        request_data["Date and time of record"] = now.strftime("%d/%m/%Y %H:%M:%S")
+        RECORDS.append(request_data)
+        print(RECORDS)
+        return jsonify({"Status": "ok"})
---
+++ sort_defs.py
@@ -0,0 +1,14 @@
+def sort_for_category(category, records):
+    category_records = []
+    for record in records:
+        if category["Category id"]==record["Category id"]:
+            category_records.append(record)
+    return category_records
+
+
+def sort_user_records(records, user):
+    user_records = []
+    for record in records:
+        if record["User id"]==user["User id"]:
+            user_records.append(record)
+    return user_records
